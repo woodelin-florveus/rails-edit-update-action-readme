b3474d59cd9adc1acc710e19b238fb477953ccdf
woodelin-florveus <woodelinflorveus@gmail.com>
2021-01-27 21:45:55 -0500

    commit tree e87116764754bda8978965a07a6ec2e621fb2728

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles, only: [:index, :show, :new, :create]
+
+  get '/articles/:id/edit', to: 'articles#edit', as: 'article_edit'
+
+  patch 'articles/:id', to: 'articles#update'
+
 end
